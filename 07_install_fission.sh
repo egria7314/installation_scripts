ace7d6e82f0f6ae0ed94428ee224cb897a8c498f
egria7314 <egria7314@gmail.com>
2018-12-26 09:58:00 +0800

    Update 07_install_fission.sh
    Update fission to v1.0

--- 07_install_fission.sh
+++ 07_install_fission.sh
@@ -5,5 +5,6 @@
 helm install --name fission --namespace fission --set serviceType=NodePort,routerServiceType=NodePort https://github.com/fission/fission/releases/download/1.0-rc1/fission-all-1.0-rc1.tgz
 
 # Install the Fission CLI
-curl -Lo fission https://github.com/fission/fission/releases/download/0.11.0/fission-cli-linux && chmod +x fission && sudo mv fission /usr/local/bin/
+# curl -Lo fission https://github.com/fission/fission/releases/download/0.11.0/fission-cli-linux && chmod +x fission && sudo mv fission /usr/local/bin/
+curl -Lo fission https://github.com/fission/fission/releases/download/1.0-rc1/fission-cli-linux && chmod +x fission && sudo mv fission /usr/local/bin/
 
