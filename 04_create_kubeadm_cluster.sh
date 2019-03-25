f255e9b75156c70ca8b0aa951f56b88783a3ff62
egria7314 <egria7314@gmail.com>
2019-03-25 19:31:48 +0800

    change to automatically get the eth0 IP

--- 04_create_kubeadm_cluster.sh
+++ 04_create_kubeadm_cluster.sh
@@ -4,7 +4,9 @@ sudo swapoff -a
 # create kubernetes cluster
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
 # specify the ip address of master
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=172.19.16.158
+hostIP=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${hostIP}
+#sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=172.19.16.158
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
